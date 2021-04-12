b9f2dca49d2b17119f21e361492e082e6b35157b
alialucas7 <lukitis2@gmail.com>
2021-04-12 00:01:50 -0300

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/anda/';
+$config['base_url'] = 'https://victoria-sastres.herokuapp.com/anda/';
 
 /*
 |--------------------------------------------------------------------------
